01f46357c46371c2c7408990b52b69b6f360ffdb
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-06-08 21:52:35 +0530

    feat: optimised the code and added few more icons

--- src/components/TechStack/Tech.jsx
+++ src/components/TechStack/Tech.jsx
@@ -1,46 +1,82 @@
 import React from "react";
-import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaGithub, FaMarkdown, FaBootstrap, FaPython,  } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt, FaGithub, FaMarkdown, FaBootstrap, FaPython, FaNodeJs } from "react-icons/fa";
 import { DiJavascript } from "react-icons/di";
-import { SiTailwindcss, SiDaisyui, SiNextdotjs, SiFirebase, SiAdobeillustrator, SiAdobephotoshop, SiFigma } from "react-icons/si";
+import { SiTailwindcss, SiDaisyui, SiNextdotjs, SiFirebase, SiAdobeillustrator, SiAdobephotoshop, SiFigma, SiMongodb, SiExpress } from "react-icons/si";
 
 function Tech({ icon }) {
-  return (
-    <div className="text-white">
-      {icon === "html" ? (
-        <FaHtml5 />
-      ) : icon === "css" ? (
-        <FaCss3Alt />
-      ) : icon === "js" ? (
-        <DiJavascript />
-      ) : icon === "tailwind" ? (
-        <SiTailwindcss />
-      ) : icon === "daisyui" ? (
-        <SiDaisyui />
-      ) : icon === "react" ? (
-        <FaReact />
-      ) : icon === "next" ? (
-        <SiNextdotjs />
-      ) : icon === "git" ? (
-        <FaGitAlt />
-      ) : icon === "github" ? (
-        <FaGithub/>
-      ) : icon === 'markdown' ? (
-        <FaMarkdown/>
-      ) : icon === 'firebase' ? (
-        <SiFirebase/>
-      ) : icon === 'bootstrap' ? (
-        <FaBootstrap/>
-      ) : icon === 'python' ? (
-        <FaPython/>
-      ) : icon === 'illustrator' ? (
-        <SiAdobeillustrator/>
-      ) : icon === 'photoshop' ? (
-        <SiAdobephotoshop/>
-      ) : icon === 'figma' ? (
-        <SiFigma/>
-      ): null}
-    </div>
-  );
+  let IconComponent = null;
+
+  switch (icon) {
+    case "html":
+      IconComponent = FaHtml5;
+      break;
+    case "css":
+      IconComponent = FaCss3Alt;
+      break;
+    case "js":
+      IconComponent = DiJavascript;
+      break;
+    case "tailwind":
+      IconComponent = SiTailwindcss;
+      break;
+    case "daisyui":
+      IconComponent = SiDaisyui;
+      break;
+    case "react":
+      IconComponent = FaReact;
+      break;
+    case "next":
+      IconComponent = SiNextdotjs;
+      break;
+    case "git":
+      IconComponent = FaGitAlt;
+      break;
+    case "github":
+      IconComponent = FaGithub;
+      break;
+    case "markdown":
+      IconComponent = FaMarkdown;
+      break;
+    case "firebase":
+      IconComponent = SiFirebase;
+      break;
+    case "bootstrap":
+      IconComponent = FaBootstrap;
+      break;
+    case "python":
+      IconComponent = FaPython;
+      break;
+    case "illustrator":
+      IconComponent = SiAdobeillustrator;
+      break;
+    case "photoshop":
+      IconComponent = SiAdobephotoshop;
+      break;
+    case "figma":
+      IconComponent = SiFigma;
+      break;
+    case "node":
+      IconComponent = FaNodeJs;
+      break;
+    case "mongo":
+      IconComponent = SiMongodb;
+      break;
+    case "express":
+      IconComponent = SiExpress;
+      break;
+    default:
+      break;
+  }
+
+  if (IconComponent) {
+    return (
+      <div className="text-white">
+        <IconComponent />
+      </div>
+    );
+  }
+  
+  return null;
 }
 
 export default Tech;
--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -3,28 +3,37 @@ import Heading from "../components/shared/Heading";
 import Tech from "../components/TechStack/Tech";
 
 function TechStack() {
+  const techStackIcons = [
+    "html",
+    "css",
+    "js",
+    "tailwind",
+    "daisyui",
+    "react",
+    "next",
+    "node",
+    "express",
+    "mongo",
+    "git",
+    "github",
+    "markdown",
+    "firebase",
+    "bootstrap",
+    "python",
+    "illustrator",
+    "photoshop",
+    "figma",
+  ];
+
   return (
-    <div>
+    <>
       <Heading>Tech Stack</Heading>
       <div className="grid grid-cols-2 sm:grid-cols-5 gap-9 py-8 w-screen ml-[30px] pr-[30px]">
-        <Tech icon={"html"}/>
-        <Tech icon={"css"}/>
-        <Tech icon={"js"}/>
-        <Tech icon={"tailwind"}/>
-        <Tech icon={"daisyui"}/>
-        <Tech icon={"react"}/>
-        <Tech icon={"next"}/>
-        <Tech icon={"git"}/>
-        <Tech icon={"github"}/>
-        <Tech icon={"markdown"}/>
-        <Tech icon={"firebase"}/>
-        <Tech icon={"bootstrap"}/>
-        <Tech icon={"python"}/>
-        <Tech icon={"illustrator"}/>
-        <Tech icon={"photoshop"}/>
-        <Tech icon={"figma"}/>
+        {techStackIcons.map((icon, index) => (
+          <Tech key={index} icon={icon} />
+        ))}
       </div>
-    </div>
+    </>
   );
 }
 
