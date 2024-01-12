73ef79d87e47ed92e9b23613c84075fbf109807a
SirSimon162 <anmolpattnayak2016@gmail.com>
2024-01-12 17:00:14 +0530

    chore: updated assets

--- src/pages/Designs.jsx
+++ src/pages/Designs.jsx
@@ -26,14 +26,14 @@ function Designs() {
           <BiggerContainer>
             <UIContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687555259/Portfolio/Designs/badam-ui_ltzuzb.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1701583689/Portfolio/Designs/badam-ui_ltzuzb_hzvgcw.jpg"
                 alt="img-Badam-UI"
               />
             </UIContainer>
             <BannerContainer>
               <Image
                 isCover
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687552986/Portfolio/Designs/HockeyWC_bw4xvb.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1701583717/Portfolio/Designs/HockeyWC_bw4xvb_z8qgck.jpg"
                 alt="img"
               />
             </BannerContainer>
@@ -41,31 +41,31 @@ function Designs() {
           <SmallerContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553038/Portfolio/Designs/world_theatre_day-01_sqfjmo.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058099/Portfolio/Designs/HappyNewYear_noqz0z.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553027/Portfolio/Designs/vector_night_scene-01_sgv9pj.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058063/Portfolio/Designs/10_days_to_go_mypzgh.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553027/Portfolio/Designs/Yoga_Day-01_lbzcyj.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058056/Portfolio/Designs/HumanRightsDay-01_xl0dbg.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553025/Portfolio/Designs/world_suicide_prevention_day-02_yrradq.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058011/Portfolio/Designs/Mental_Health_Day-02_qnfh6v.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553018/Portfolio/Designs/Inter_NIT_g9iwwb.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058052/Portfolio/Designs/Independence_Day_CodeHub-01_mx35h5.png"
                 alt="img"
               />
             </PosterContainer>
@@ -75,14 +75,14 @@ function Designs() {
           <BiggerContainer>
             <UIContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687555259/Portfolio/Designs/cest-ui_wvimcu.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1701583674/Portfolio/Designs/cest-ui_wvimcu_ho7pll.jpg"
                 alt="img-CEST-UI"
               />
             </UIContainer>
             <BannerContainer>
               <Image
                 isCover
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553038/Portfolio/Designs/Sahibzade-01_yyh8bd.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1701583773/Portfolio/Designs/Sahibzade-01_yyh8bd_jox6aa.jpg"
                 alt="img"
               />
             </BannerContainer>
@@ -90,31 +90,31 @@ function Designs() {
           <SmallerContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553017/Portfolio/Designs/Independence_Day_CodeHub-01_rfrnsn.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058058/Portfolio/Designs/C4GT_poster_GitHub-04_sseld4.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687553008/Portfolio/Designs/Mental_Health_Day-02_odi9kb.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705057995/Portfolio/Designs/world_theatre_day-01_ck167k.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687552999/Portfolio/Designs/HumanRightsDay-01_dg2dvy.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705057990/Portfolio/Designs/Yoga_Day-01_fhpeuh.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687552990/Portfolio/Designs/vriddhi-01_q89wjx.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705057983/Portfolio/Designs/world_suicide_prevention_day-02_montti.png"
                 alt="img"
               />
             </PosterContainer>
             <PosterContainer>
               <Image
-                src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1687552988/Portfolio/Designs/AutoCad_Workshop-01_hjhlam.jpg"
+                src="https://res.cloudinary.com/darx97f61/image/upload/v1705058040/Portfolio/Designs/INTER-NIT_banner-03_keazr4.png"
                 alt="img"
               />
             </PosterContainer>
